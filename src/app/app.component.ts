ce0ed1b9cd52d2d629423ac9540a4eee36a62393
Ednaldo Romer de Oliveira Júnior <ednaldojunior2001@gmail.com>
2024-02-14 23:42:36 -0300

    Adding the youtube player

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { YouTubePlayer } from '@angular/youtube-player';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, YouTubePlayer],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'timeless-webapp';
+
+  playerVars = {autoplay: 1}
 }
